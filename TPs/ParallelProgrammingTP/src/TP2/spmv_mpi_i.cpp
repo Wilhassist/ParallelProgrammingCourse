6caecb8437d3ffadd947a588f70e0f986794f433
Nelly Wilhelmine WOKAM BOUNSI <nelly-wilhelmine.wokam_bounsi@mines-paristech.fr>
2024-12-11 11:25:35 +0100

    "."

--- TPs/ParallelProgrammingTP/src/TP2/spmv_mpi_i.cpp
+++ TPs/ParallelProgrammingTP/src/TP2/spmv_mpi_i.cpp
@@ -79,11 +79,11 @@ void scatterCSRMatrix(
     
     MPI_Scatterv(
         full_data.kcol.data(), 
-        row_counts.data(), 
+        row_counts.data() + 1, 
         row_displs.data(), 
         MPI_INT, 
         local_data.kcol.data(), 
-        row_counts[rank], 
+        row_counts[rank] + 1,  
         MPI_INT, 
         0, comm
     );
