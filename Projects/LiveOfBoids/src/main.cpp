d830c8daa91cf12244d978395651d2ec7973dec3
Nelly Wilhelmine WOKAM BOUNSI <nelly-wilhelmine.wokam_bounsi@mines-paristech.fr>
2024-12-17 14:53:02 +0100

    "."

--- Projects/LiveOfBoids/src/main.cpp
+++ Projects/LiveOfBoids/src/main.cpp
@@ -21,11 +21,6 @@ int main(int argc, char* argv[]) {
     agentFactory.createEagles();
     agentFactory.createPreys();
     
-    // Initialize FPS tracking variables
-    long count = 0;
-    double accumulated_time = 0;
-    auto last_time = std::chrono::steady_clock::now();
-
     // global loop
     Grid grid(agentFactory.ratio() * agentFactory.width(), agentFactory.ratio() * agentFactory.height(), config.cellSize, config.margin, config.mode);
 
@@ -34,14 +29,12 @@ int main(int argc, char* argv[]) {
         grid.addBoid(agent.get()); 
     }
 
+    auto last_time = std::chrono::steady_clock::now();
     grid.updateBoids();
-
     auto now = std::chrono::steady_clock::now();
     auto duration = std::chrono::duration<double>(now - last_time).count();
 
-    auto fps = static_cast<double>(count) / accumulated_time;
-
-    std::cout<<"Time: "<< fps<<std::endl;
+    std::cout<<"Time: "<< duration<<std::endl;
 
     exit(EXIT_SUCCESS);
 }
